648c4e80caffa9ee83214a1290ffc7b5ab3e2b9b
Dmytro Ohanov <dmitryohanovphotography@gmail.com>
2023-07-19 01:52:48 +0100

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'plant': "url('https://images.pexels.com/photos/916339/pexels-photo-916339.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')",
+      }
+    }
+  },
+  plugins: [],
+}
